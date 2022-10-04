672a7d8db48e7be834ba3c174831de4061a523b3
ryuke <nidhalnaffati@gmail.com>
2022-10-04 21:11:27 +0100

    i changed this class file to UserAlreadyExistsException so i can throw it in both cases existing the same email or the same id. and i added NoSuchElementException.

---
+++ src/main/java/com/CarRentalAgency/exception/NoSuchElementException.java
@@ -0,0 +1,7 @@
+package com.CarRentalAgency.exception;
+
+public class NoSuchElementException extends RuntimeException {
+    public NoSuchElementException(String message) {
+        super(message);
+    }
+}
