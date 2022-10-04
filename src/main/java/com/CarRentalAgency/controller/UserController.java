4f9f2294df07369d3b40f662b69b7f43166e8de9
ryuke <nidhalnaffati@gmail.com>
2022-10-04 21:02:19 +0100

    adding an exception for the list() method in the controller (excepting added to handel an empty list of users)

--- src/main/java/com/CarRentalAgency/controller/UserController.java
+++ src/main/java/com/CarRentalAgency/controller/UserController.java
@@ -2,10 +2,10 @@ package com.CarRentalAgency.controller;
 
 
 import com.CarRentalAgency.entity.User;
+import com.CarRentalAgency.exception.NoSuchElementException;
 import com.CarRentalAgency.exception.UserNotFoundException;
 import com.CarRentalAgency.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,43 +13,43 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController()
+@RequestMapping("api/v1/user")
 public class UserController {
 
     @Autowired
     UserServiceImpl userService;
 
-    @GetMapping("/user/list")
+    @GetMapping("/list")
     public List<User> list() {
         List<User> listUsers = userService.findAll();
+        if(listUsers.isEmpty()) throw new NoSuchElementException("THERE IS NO USER IN THE DATA BASE.");
         return listUsers;
     }
 
-    @GetMapping("user/list/name/{name}")
-    public User fetchUserByName(@PathVariable("name") String userName) throws UserNotFoundException {
-        User user = userService.findByFirstNameIgnoreCase(userName);
-        return user;
+    @GetMapping("/list/name/{name}")
+    public List<User> fetchUserByName(@PathVariable("name") String userName) throws UserNotFoundException {
+        List<User> userList = userService.findByFirstNameIgnoreCase(userName);
+        return userList;
     }
 
 
-    @GetMapping("/user/list/id/{id}")
+    @GetMapping("/list/id/{id}")
     public User fetchUserByID(@PathVariable("id") Long id) throws UserNotFoundException {
-        return userService.findById(id);
+        return userService.findById(id).get();
     }
 
-    @PostMapping("/user/save")
-    public User saveUser(@Valid @RequestBody User user) {
+    @PostMapping("/save")
+    public User saveUser(@Valid @RequestBody User user) throws UserNotFoundException {
         return userService.saveUser(user);
     }
 
-    @DeleteMapping("user/delete/{id}")
-    public String deleteUserById(@PathVariable Long id) {
-        // I should handel the wrong id from the user . ( EmptyResultDataAccessException )
-
+    @DeleteMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable Long id) throws NoSuchElementException {
         userService.deleteById(id);
         return "User Deleted Succesfuly ;) ";
     }
 
-    @PutMapping("user/update/{id}")
+    @PutMapping("/update/{id}")
     public User updateUserById(@PathVariable Long id, @RequestBody @Valid User user) throws UserNotFoundException {
         return userService.updateUser(id, user);
     }
