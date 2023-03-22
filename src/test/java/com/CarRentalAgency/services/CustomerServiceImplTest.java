a33233cfe226c79a2954d3228704c1735f8b0f9b
ryuke <nidhalnaffati@gmail.com>
2023-03-22 22:48:41 +0100

    This commit adds a JUnit test class for the `CustomerServiceImpl` class, named `CustomerServiceImplTest`. The test class contains test methods to cover all public methods of the `CarServiceImpl`, achieving 100% method coverage and 100% of line coverage.

--- src/test/java/com/CarRentalAgency/services/CustomerServiceImplTest.java
+++ src/test/java/com/CarRentalAgency/services/CustomerServiceImplTest.java
@@ -1,26 +1,25 @@
 package com.CarRentalAgency.services;
 
 import com.CarRentalAgency.entity.Customer;
+import com.CarRentalAgency.exception.AlreadyExistsException;
 import com.CarRentalAgency.repository.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
 
     /**
@@ -46,6 +45,8 @@ class CustomerServiceImplTest {
     @BeforeEach
     void setUp() {
 
+        underTestService = new CustomerServiceImpl(customerRepository);
+
         customer1 = Customer.builder()
                 .id(1L)
                 .email("myEmail1@gmail.com")
@@ -63,7 +64,7 @@ class CustomerServiceImplTest {
 
 
     @Test
-    void saveCustomer() {
+    void testSaveCustomer_ShouldSuccess() {
         // resources: https://stackoverflow.com/questions/63526353/unit-test-for-save-method-of-jpa-repository-in-spring-boot
 
         //given
@@ -72,121 +73,264 @@ class CustomerServiceImplTest {
         underTestService.saveCustomer(testCustomer);
         //then
         verify(customerRepository, times(1)).save(any());
-
     }
 
     @Test
-    @DisplayName("Find All Customers in the DB")
-    void findAll_GivenAListAsParam_ExpectingTheSameList() {
+    void testSaveCustomer_ShouldThrowsAlreadyExistsException() {
 
-        List<Customer> customerList = Arrays.asList(customer1, customer2);
-
-        given(customerRepository.findAll())
-                .willReturn(customerList);
-
-        assertThat(underTestService.findAllCustomers())
-                .isEqualTo(Arrays.asList(customer1, customer2));
-
-        assertThat(underTestService.findAllCustomers())
-                .hasSize(2)
-                .contains(customer1, customer2);
-    }
-
-    @Test()
-    void findCustomerById_ShouldReturnCustomer() {
-
-        // finding an existing Customer by his ID.
-        given(customerRepository.findById(1L))
-                .willReturn(Optional.ofNullable(customer1));
-
-        assertThat(underTestService.findCustomerById(1L))
-                .isEqualTo(Optional.ofNullable(customer1));
-
-    }
-
-    @Test()
-    void findById_givenWrongId_ShouldThrowException() {
-
-        Long wrongID = 999_999L;
-
-        // finding a non-existing Customer by his email.
-
-        //this will throw NoSuchElementException.
-        assertThatExceptionOfType(NoSuchElementException.class)
-                .isThrownBy(() -> underTestService.findCustomerById(wrongID))
-                .withMessage("THERE IS NO CUSTOMER WITH THIS ID: 999999");
-
-    }
-
-    @Test
-    void findCustomerByEmail_ShouldReturnARecord_OrThrowException() {
-
-        // finding an existing Customer by his email.
-        given(customerRepository.findCustomerByEmail("myEmail1@gmail.com"))
-                .willReturn(Optional.ofNullable(customer1));
-
-        assertThat(underTestService.findCustomerByEmail("myEmail1@gmail.com"))
-                .isEqualTo(Optional.ofNullable(customer1));
-
-    }
-
-
-    @Test
-    void findCustomerByEmail_ShouldThrowException() {
-
-        assertThatExceptionOfType(NoSuchElementException.class)
-                .isThrownBy(() -> underTestService.findCustomerByEmail("thisEmailTotalyUnFoundInTheDB"));
-    }
-
-
-    @Test
-    void findCustomerByFirstName_ShouldRetunRecord() {
         //given
-        final String firstName = "fname";
-        final String reversedFirstName = "Fname";
-
-        customer1.setFirstName(firstName);
-
-        customerList = Arrays.asList(customer1);
+        when(customerRepository.findCustomerByEmail(anyString()))
+                .thenReturn(Optional.of(customer1));
 
         //when
-        when(customerRepository
-                .findByFirstNameIgnoreCase(firstName)).thenReturn(customerList);
+        AlreadyExistsException exception = assertThrows(AlreadyExistsException.class, () -> {
+            underTestService.saveCustomer(customer1);
+        });
 
         //then
+        verify(customerRepository, times(1)).findCustomerByEmail(anyString());
+        verify(customerRepository, times(0)).save(any());
+        Assertions.assertEquals("THIS EMAIL:" + customer1.getEmail() + " ALREADY USED !!", exception.getMessage());
+    }
 
-        assertThat(underTestService.
-                findCustomerByFirstNameIgnoreCase(reversedFirstName))
-                .contains(customer1)
-                .hasSize(1);
+    @Test
+    void testDeleteCustomer_ShouldDeleteTheCustomer() {
+
+        //given
+        long existingCustomerID = customer1.getId();
+
+        //when
+        when(customerRepository.findById(existingCustomerID))
+                .thenReturn(Optional.of(customer1));
+        doNothing().when(customerRepository).deleteById(existingCustomerID);
+
+        //then
+        assertDoesNotThrow(() -> underTestService.deleteCustomerById(existingCustomerID));
+        verify(customerRepository, times(1))
+                .deleteById(existingCustomerID);
+    }
+
+    @Test
+    void testDeleteCustomer_ShouldThrowNoSuchElementException() {
+
+        //given
+        Customer customer = customer1;
+        long nonExistingCustomerID = customer.getId();
+        final String expectedExceptionMessage =
+                "CANNOT DELETE A NON-EXISTENT CUSTOMER WITH THIS ID: " + nonExistingCustomerID + " PROBABLY IT DOESNT EXIST OR HE IS ALREADY DELETED ;(";
+
+        when(customerRepository.findById(nonExistingCustomerID))
+                .thenReturn(Optional.empty());
+
+        //when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            underTestService.deleteCustomerById(nonExistingCustomerID);
+        });
+
+        //then
+        verify(customerRepository, times(1)).findById(anyLong());
+        verify(customerRepository, times(0)).deleteById(anyLong());
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void testUpdateCustomer_ShouldSuccess() {
+        // given
+        Customer newCustomer = customer2;
+
+        when(customerRepository.existsById(1L)).thenReturn(true);
+        when(customerRepository.findCustomerById(1L)).thenReturn(customer1);
+        when(customerRepository.save(customer1)).thenReturn(customer1);
+
+        // when
+        Customer updatedCustomer = underTestService.updateCustomer(1L, newCustomer);
+
+        // then
+        verify(customerRepository, times(1)).existsById(anyLong());
+        verify(customerRepository, times(1)).save(any());
+
+        assertNotNull(updatedCustomer);
+
+        assertEquals(customer1.getId(), updatedCustomer.getId());
+        assertEquals(newCustomer.getEmail(), updatedCustomer.getEmail());
+        assertEquals(newCustomer.getFirstName(), updatedCustomer.getFirstName());
+        assertEquals(newCustomer.getLastName(), updatedCustomer.getLastName());
+    }
+
+    @Test
+    void testUpdateCustomer_ShouldThrowNoSuchElementException() {
+        // given
+        long nonExistingId = 999_999L;
+        final String expectedExceptionMessage = "user id: " + nonExistingId + " doesnt exist!!";
+        when(customerRepository.existsById(nonExistingId))
+                .thenReturn(false);
+
+        // when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            underTestService.updateCustomer(nonExistingId, customer1);
+        });
+
+        // then
+        verify(customerRepository, times(1)).existsById(anyLong());
+        verify(customerRepository, times(0)).save(any());
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
 
     }
 
-
     @Test
-    void deletingCustomer_retrievesCustomerByID() {
+    void testUpdateCustomer_ShouldThrowAlreadyExistsException() {
+        // given
+        Long existingId = 1L;
+        Customer newCustomer = customer2;
+        String expectedExceptionMessage = "this email: " + newCustomer.getEmail() + " already exists !!";
 
-        when(customerRepository.findById(1L)).thenReturn(Optional.of(customer1));
+        when(customerRepository.existsById(existingId)).thenReturn(true);
+        when(customerRepository.findCustomerById(existingId)).thenReturn(customer1);
+        when(customerRepository.findCustomerByEmail(newCustomer.getEmail())).thenReturn(Optional.of(customer2));
 
-        underTestService.deleteCustomerById(1L);
+        // when
+        AlreadyExistsException exception = assertThrows(AlreadyExistsException.class,
+                () -> {
+                    underTestService.updateCustomer(existingId, newCustomer);
+                });
 
-        verify(customerRepository, times(1)).deleteById(eq(1L));
-
+        // then
+        verify(customerRepository, times(1)).existsById(anyLong());
+        verify(customerRepository, times(0)).save(any());
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
     }
 
-
     @Test
-    void deletingCustomer_ShouldThrowNoSuchElementException() {
+    void testFindAllCustomers_ShouldReturnAListOfCustomers() {
 
-        // resources: https://stackoverflow.com/questions/69109025/how-do-you-assert-with-delete-method-void
+        //given
+        List<Customer> customerList = Arrays.asList(customer1, customer2);
 
-        final String OUTPUT_MESSAGE = "CANNOT DELETE NON-EXISTING CUSTOMER, THIS ID: 1 DOESNT EXIST OR HE IS ALREADY DELETED.";
+        //when
+        when(customerRepository.findAll()).thenReturn(customerList);
 
+        //then
+        assertThat(underTestService.findAllCustomers())
+                .contains(customer1, customer2)
+                .size().isEqualTo(2);
+    }
+
+    @Test()
+    void testFindCustomerById_ShouldReturnCustomer() {
+
+        //given
+        Customer existingCustomer = customer1;
+        long id = existingCustomer.getId();
+
+        //when
+        when(customerRepository.findById(id)).thenReturn(Optional.of(existingCustomer));
+
+        //then
+        assertThat(underTestService.findCustomerById(id))
+                .isEqualTo(existingCustomer);
+    }
+
+    @Test()
+    void testFindById_ShouldThrowException() {
+
+        //given
+        Long wrongID = 999_999L;
+        String expectedExceptionMessage = "THERE IS NO CUSTOMER WITH THIS ID: " + wrongID;
+
+        //when
+        when(customerRepository.findById(wrongID))
+                .thenReturn(Optional.empty());
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            underTestService.findCustomerById(wrongID);
+        });
+
+        //then
         assertThatExceptionOfType(NoSuchElementException.class)
-                .isThrownBy(() -> underTestService.deleteCustomerById(1L))
-                .withMessage(OUTPUT_MESSAGE);
+                .isThrownBy(() -> underTestService.findCustomerById(wrongID));
+
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void testFindCustomerByEmail_ShouldReturnCustomer() {
+
+        // given
+        Customer existingCustomer = customer1;
+        String email = existingCustomer.getEmail();
 
 
+        // when
+        when(customerRepository.findCustomerByEmail(email))
+                .thenReturn(Optional.of(existingCustomer));
+
+
+        // then
+        assertThat(underTestService.findCustomerByEmail(email))
+                .isEqualTo(existingCustomer);
+
+        verify(customerRepository, times(1)).findCustomerByEmail(anyString());
+    }
+
+
+    @Test
+    void testFindCustomerByEmail_ShouldThrowNoSuchElementException() {
+        //given
+        String email = "nonExistentEmail@gmail.com";
+        String expectedExceptionMessage = "THE IS NO CUSTOMER WITH THIS EMAIL :" + email +
+                                          " MAYBE YOU MEAN : [existentEmail@gmail.com, anotherExistentEmail@gmail.com]";
+
+        when(customerRepository.findCustomerByEmail(email))
+                .thenReturn(Optional.empty());
+
+        when(customerRepository.approximateEmails(email))
+                .thenReturn(List.of("existentEmail@gmail.com", "anotherExistentEmail@gmail.com"));
+
+        //when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+                () -> underTestService.findCustomerByEmail(email));
+
+        //then
+        assertEquals(expectedExceptionMessage, exception.getMessage());
+
+    }
+
+
+    @Test
+    void testFindCustomerByFirstNameIgnoreCase_ShouldReturnCustomers() {
+        //given
+        String firstName = "myfirstname1";
+        when(customerRepository.findByFirstNameIgnoreCase(firstName))
+                .thenReturn(List.of(customer1));
+
+        //when
+        List<Customer> foundCustomers = underTestService.findCustomerByFirstNameIgnoreCase(firstName);
+
+        //then
+        assertThat(foundCustomers).containsExactly(customer1);
+    }
+
+    @Test
+    void testFindCustomerByFirstNameIgnoreCase_ShouldThrowNoSuchElementException() {
+        //given
+        String firstName = "nonExistentName";
+        String expectedExceptionMessage = "OOPS THIS CUSTOMER NAME DOESNT EXIST !! " +
+                                          " MAYBE YOU MEAN : [existentName]";
+
+        when(customerRepository.findByFirstNameIgnoreCase(firstName))
+                .thenReturn(Collections.emptyList());
+
+        when(customerRepository.approximateNames(firstName))
+                .thenReturn(List.of("existentName"));
+
+        //when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+                () -> underTestService.findCustomerByFirstNameIgnoreCase(firstName));
+
+        //then
+        assertEquals(expectedExceptionMessage, exception.getMessage());
+        verify(customerRepository).findByFirstNameIgnoreCase(firstName);
+        verify(customerRepository).approximateNames(firstName);
     }
 
 }
