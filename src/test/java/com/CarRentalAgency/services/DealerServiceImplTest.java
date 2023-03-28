94cd60d2983932938e40128c2a00d8d0f6c3f8c4
ryuke <nidhalnaffati@gmail.com>
2023-03-28 19:50:51 +0100

    Write the test class for the DealerServiceImpl.java

--- src/test/java/com/CarRentalAgency/services/DealerServiceImplTest.java
+++ src/test/java/com/CarRentalAgency/services/DealerServiceImplTest.java
@@ -2,11 +2,10 @@ package com.CarRentalAgency.services;
 
 import com.CarRentalAgency.entity.Car;
 import com.CarRentalAgency.entity.Dealer;
-
+import com.CarRentalAgency.exception.AlreadyExistsException;
 import com.CarRentalAgency.repository.DealerRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,8 +14,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,10 +37,12 @@ class DealerServiceImplTest {
 
     private Dealer dealer1;
     private Dealer dealer2;
-    private List<Car> carList = new ArrayList<>();
+    private final List<Car> carList = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
+
+        underTestService = new DealerServiceImpl(dealerRepository);
 
         dealer1 = Dealer.builder()
                 .id(1L)
@@ -59,9 +62,145 @@ class DealerServiceImplTest {
 
     }
 
+    @Test
+    void saveDealer_withNewEmail_shouldSaveDealerAndReturnIt() {
+        // given
+        Dealer dealerToSave = dealer1;
+        String email = dealerToSave.getEmail();
+        when(dealerRepository.findByEmail(email)).thenReturn(Optional.empty());
+        when(dealerRepository.save(dealerToSave)).thenReturn(dealerToSave);
+
+
+        // when
+        Dealer savedDealer = underTestService.saveDealer(dealerToSave);
+
+        // then
+        verify(dealerRepository, times(1)).save(dealerToSave);
+        assertNotNull(savedDealer);
+        assertEquals(dealerToSave.getEmail(), savedDealer.getEmail());
+        assertEquals(dealerToSave.getFirstName(), savedDealer.getFirstName());
+        assertEquals(dealerToSave.getLastName(), savedDealer.getLastName());
+    }
 
     @Test
-    void findAll_GivenAListAsParam_ExpectingTheSameList() {
+    void saveDealer_withExistingEmail_shouldThrowAlreadyExistsException() {
+        // given
+        Dealer existingDealer = dealer1;
+        Dealer newDealer = dealer2;
+        String newDealerEmail = newDealer.getEmail();
+        when(dealerRepository.findByEmail(newDealerEmail)).thenReturn(Optional.of(existingDealer));
+
+        // when
+        DealerServiceImpl dealerService = new DealerServiceImpl(dealerRepository);
+
+        // then
+        assertThrows(AlreadyExistsException.class, () -> dealerService.saveDealer(newDealer));
+    }
+
+
+    @Test
+    void testUpdateDealer_ShouldSuccess() {
+        // given
+        Dealer newDealer = dealer2;
+        Dealer existingDealer = dealer1;
+        Long idForTheExistingDealer = dealer1.getId();
+
+        when(dealerRepository.findById(idForTheExistingDealer))
+                .thenReturn(Optional.of(existingDealer));
+
+        when(dealerRepository.findByEmail(newDealer.getEmail()))
+                .thenReturn(Optional.empty());
+
+        // when
+        Dealer updatedDealer = underTestService.updateDealer(idForTheExistingDealer, newDealer);
+
+        // then
+        assertNotNull(updatedDealer);
+        assertEquals(newDealer.getEmail(), updatedDealer.getEmail());
+        assertEquals(newDealer.getFirstName(), updatedDealer.getFirstName());
+        assertEquals(newDealer.getLastName(), updatedDealer.getLastName());
+    }
+
+    @Test
+    void testUpdateDealer_ShouldThrowNoSuchElementException() {
+        // given
+        Dealer updatedDealer = dealer1;
+        String expectedMessage = "Dealer with id " + 1L + " does not exist";
+        when(dealerRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> underTestService.updateDealer(1L, updatedDealer));
+
+        //then
+        assertEquals(expectedMessage, exception.getMessage());
+        verify(dealerRepository, times(0)).save(updatedDealer);
+    }
+
+    @Test
+    void testUpdateDealer_ShouldThrowAlreadyExistsException() {
+        // given
+        Dealer newDealer = dealer2;
+        Dealer existingDealer = dealer1;
+        Long idForTheExistingDealer = dealer1.getId();
+        String expectedMessage = "this email: " + newDealer.getEmail() + " already exists !!";
+
+        when(dealerRepository.findById(idForTheExistingDealer))
+                .thenReturn(Optional.of(existingDealer));
+
+        when(dealerRepository.findByEmail(newDealer.getEmail()))
+                .thenReturn(Optional.of(existingDealer));
+
+        // when
+        Exception AlreadyExistsException = assertThrows(AlreadyExistsException.class, () -> underTestService.updateDealer(idForTheExistingDealer, newDealer));
+
+
+        // then
+        assertEquals(expectedMessage, AlreadyExistsException.getMessage());
+        verify(dealerRepository, times(0)).save(newDealer);
+    }
+
+    @Test
+    void testDeletingDealer_ShouldDeleteIt() {
+
+        // given - precondition or setup
+        long existingCustomerID = dealer1.getId();
+
+        when(dealerRepository.findById(existingCustomerID))
+                .thenReturn(Optional.of(dealer1));
+
+        willDoNothing().given(dealerRepository).deleteById(existingCustomerID);
+
+        // when -  action or the behaviour that we are going test
+        underTestService.deleteDealerById(existingCustomerID);
+
+        // then - verify the output
+        verify(dealerRepository, times(1)).deleteById(existingCustomerID);
+    }
+
+    @Test
+    void testDeleteDealer_ShouldThrowNoSuchElementException() {
+        //given
+        Dealer dealer = dealer1;
+        long nonExistingCustomerID = dealer.getId();
+        final String expectedExceptionMessage =
+                "CANNOT DELETE A NON-EXISTENT DEALER WITH THIS ID: " + nonExistingCustomerID
+                + " PROBABLY IT DOESNT EXIST OR HE IS ALREADY DELETED ;(";
+
+        when(dealerRepository.findById(nonExistingCustomerID))
+                .thenReturn(Optional.empty());
+
+        //when
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class,
+                () -> underTestService.deleteDealerById(nonExistingCustomerID));
+
+        //then
+        verify(dealerRepository, times(1)).findById(anyLong());
+        verify(dealerRepository, times(0)).deleteById(anyLong());
+        Assertions.assertEquals(expectedExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void testFindAll_GivenAListAsParam_ExpectingTheSameList() {
 
 
         List<Dealer> dealerList = Arrays.asList(dealer1, dealer2);
@@ -79,7 +218,7 @@ class DealerServiceImplTest {
     }
 
     @Test()
-    void findById_GivenId_ShouldReturnRecord() {
+    void testFindById_GivenId_ShouldReturnRecord() {
 
         given(dealerRepository.findById(1L))
                 .willReturn(Optional.ofNullable(dealer1));
@@ -90,7 +229,7 @@ class DealerServiceImplTest {
     }
 
     @Test()
-    void findById_GivenWrongId_ShouldThrowException() {
+    void testFindById_GivenWrongId_ShouldThrowException() {
 
         Long wrongID = 999_999_999L;
 
@@ -102,7 +241,7 @@ class DealerServiceImplTest {
     }
 
     @Test
-    void findByEmail_GivenExistingEmail_ShouldReturnTheCorrectDealer() {
+    void testFindByEmail_GivenExistingEmail_ShouldReturnTheCorrectDealer() {
 
 
         given(dealerRepository.findByEmail(dealer1.getEmail()))
@@ -114,7 +253,7 @@ class DealerServiceImplTest {
     }
 
     @Test
-    void findByEmail_givenWrongEmail_ShouldThrowException() {
+    void testFindByEmail_givenWrongEmail_ShouldThrowException() {
 
         String wrongEmail = "wrongEmail@gmail.com";
 
@@ -123,65 +262,45 @@ class DealerServiceImplTest {
         );
     }
 
-
     @Test
-    void SaveDealer_IfExists_OrThrowException() {
-        //given
-        given(dealerRepository.save(dealer1))
-                .willReturn(dealer1);
+    void testFindDealerByFirstNameIgnoreCase_ShouldSuccess() {
+        // given
+        String firstName = "John";
+        String firstNameUpperCase = "JOHN";
+        dealer1.setFirstName(firstName);
+        dealer2.setFirstName(firstNameUpperCase);
 
-        // when -  action or the behaviour that we are going test
-        dealer1 = underTestService.saveDealer(dealer1);
+        List<Dealer> dealers = Arrays.asList(dealer1, dealer2);
+        when(dealerRepository.findByFirstNameIgnoreCase(firstName)).thenReturn(dealers);
 
+        // when
+        List<Dealer> foundDealers = underTestService.findDealerByFirstNameIgnoreCase(firstName);
 
-        // then - verify the output
-        assertThat(dealer1).isNotNull();
-
+        // then
+        assertNotNull(foundDealers);
+        assertEquals(dealers.size(), foundDealers.size());
+        assertEquals(dealers.get(0).getFirstName(), foundDealers.get(0).getFirstName());
+        assertEquals(dealers.get(1).getFirstName(), foundDealers.get(1).getFirstName());
     }
 
     @Test
-    @Disabled
-    void deletingDealer_IfExists() {
+    void testFindDealerByFirstNameIgnoreCase_ShouldThrowNoSuchElementException() {
+        // given
+        when(dealerRepository.findByFirstNameIgnoreCase("joh"))
+                .thenReturn(List.of());
 
-        dealer1 = underTestService.saveDealer(dealer1);
+        List<String> dealersNames = List.of("John", "JOHN");
+        when(dealerRepository.approximateNames("joh")).thenReturn(dealersNames);
 
-        // given - precondition or setup
-        Long correcID = 1L;
+        // when
+        NoSuchElementException exception = assertThrows(
+                NoSuchElementException.class,
+                () -> underTestService.findDealerByFirstNameIgnoreCase("joh")
+        );
 
-
-        willDoNothing().given(dealerRepository).deleteById(correcID);
-
-        // when -  action or the behaviour that we are going test
-        underTestService.deleteDealerById(correcID);
-
-        // then - verify the output
-        verify(dealerRepository, times(1)).deleteById(correcID);
-
-    }
-
-    @Test
-    @Disabled
-    void deletingDealer_IfIdDoesntExists_ThrowException() {
-
-        Long correctID = 9999L;
-
-        List<Car> carList = new ArrayList<>();
-
-        Dealer expected = Dealer.builder()
-                .id(correctID)
-                .email("myEmail@gmail.com")
-                .firstName("myFirstName")
-                .lastName("myLastName")
-                .carList(carList)
-                .build();
-
-
-        underTestService.saveDealer(expected);
-
-        underTestService.deleteDealerById(expected.getId());
-
-        verify(dealerRepository).delete(expected);
-
+        // then
+        assertThat(dealersNames).contains("John", "JOHN");
+        assertThat(dealersNames).size().isEqualTo(2);
     }
 
 }
